988b62b59abff25bce58d26250d19aff596925d6
Momir Stanišić <momir.stanisic.ns@gmail.com>
2024-02-26 07:18:06 +0100

    checkpoint

--- client/script.js
+++ client/script.js
@@ -10,9 +10,31 @@ function showFirstPage(show) {
     document.getElementById("addPageSvg").style.fill = !show ? "white" : "#999";
 }
 
-function resize() {
-    document.getElementById("footer").style.display = window.innerHeight > 550 ? "inherit" : "none";
-}
+let socket = new WebSocket('ws://localhost:8080');
+
+socket.onopen = function (e) {
+    console.log("[open] Connection established");
+    console.log("Sending to server");
+    socket.send("My name is John");
+};
+
+socket.onmessage = function (event) {
+    console.log(`[message] Data received from server: ${event.data}`);
+};
+
+socket.onclose = function (event) {
+    if (event.wasClean) {
+        console.log(`[close] Connection closed cleanly, code=${event.code} reason=${event.reason}`);
+    } else {
+        // e.g. server process killed or network down
+        // event.code is usually 1006 in this case
+        console.log('[close] Connection died');
+    }
+};
+
+socket.onerror = function (error) {
+    console.log(`[error]`);
+};
 
 function pickFromQueue() {
 
--- server/main.js
+++ server/main.js
@@ -1,9 +1,16 @@
-var http = require("http");
+const { WebSocket } = require('ws');
 
-//create a server object:
-http
-  .createServer(function (req, res) {
-    res.write("Hello World!"); //write a response to the client
-    res.end(); //end the response
-  })
-  .listen(8080); //the server object listens on port 8080
+const wss = new WebSocket.Server({ port: 8080 });
+
+wss.on('connection', (ws) => {
+  console.log('New client connected');
+
+  ws.on('message', (message) => {
+    console.log(`Received message: ${message}`);
+    ws.send(`Server received your message: ${message}`);
+  });
+
+  ws.on('close', () => {
+    console.log('Client disconnected');
+  });
+});
